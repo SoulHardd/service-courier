f330f3356e253cb0b86f8e0266e07a19d2c7b343
SoulHardd <vladbogatyrenko@gmail.com>
2026-01-25 23:04:38 +0300

    fix

--- migrations/20260121175902_create_indexes.sql
+++ migrations/20260121175902_create_indexes.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE UNIQUE INDEX ux_couriers_phone ON couriers(phone);
-CREATE INDEX idx_couriers_status ON couriers(status);
 
 CREATE UNIQUE INDEX ux_delivery_order_id ON delivery(order_id);
 CREATE INDEX idx_delivery_courier_deadline ON delivery(courier_id, deadline);
@@ -12,6 +11,5 @@ CREATE INDEX idx_delivery_courier_deadline ON delivery(courier_id, deadline);
 DROP INDEX IF EXISTS idx_delivery_courier_deadline;
 DROP INDEX IF EXISTS ux_delivery_order_id;
 
-DROP INDEX IF EXISTS idx_couriers_status;
 DROP INDEX IF EXISTS ux_couriers_phone;
 -- +goose StatementEnd
